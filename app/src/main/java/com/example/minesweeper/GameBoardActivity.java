f9af2519d4574a4bed3288399094f002100f682c
Zuki0612 <yuzuki.20030612@gmail.com>
2025-04-27 23:54:19 -0400

    first commit from zuki

--- app/src/main/java/com/example/minesweeper/GameBoardActivity.java
+++ app/src/main/java/com/example/minesweeper/GameBoardActivity.java
@@ -1,16 +1,29 @@
 package com.example.minesweeper;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.GridLayout;
+
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class GameBoardActivity extends AppCompatActivity {
 
     public static String soloLevel = PlaySoloActivity.level;
     private Button[][] gameBoard;
+    private int[][] board;
+    private int BOMB_NUMBER;
     private int bsize = 5;
 
     //dimensions for each board
@@ -22,15 +35,48 @@ public class GameBoardActivity extends AppCompatActivity {
 
     private static final int arow = 30;
     private static final int acol = 16;
+    private static int ROW;
+    private static int COLUMN;
+    private static Boolean started;
+    int[] seconds;
+    LinearLayout holdBoard;
+    Handler handler;
+    Runnable runnable;
+    TextView timer;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.board_template);
+        started = false;
+        // Get the level from Intent
+        String soloLevel = getIntent().getStringExtra("level");
         chooseBoard(soloLevel);
 
+        // Popup page after player clicked a bomb or player won the game
+        LinearLayout gameOverPopup = findViewById(R.id.GameOverPopup);
+        Button againBtn = findViewById(R.id.btnAgain);
+        Button backBtn = findViewById(R.id.btnBack);
+        againBtn.setOnClickListener(this::OnClickAgain);
+        backBtn.setOnClickListener(this::OnClickBack);
+
+        // for timer
+        seconds = new int[]{0};
+        timer = findViewById(R.id.timer);
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                seconds[0]++;
+                int totalSeconds = seconds[0];
+                int minutes = totalSeconds / 60;
+                int sec = totalSeconds % 60;
+                String timeFormatted = String.format("%d:%02d", minutes, sec);
+                timer.setText(timeFormatted);
+                handler.postDelayed(this, 1000);  // Repeat every 1 second
+            }
+        };
     }
 
 
@@ -39,42 +85,254 @@ public class GameBoardActivity extends AppCompatActivity {
     public void chooseBoard(String level){
         switch(level){
             case "beginner":
-                buildBoard(brow, bcol);
+                ROW = brow;
+                COLUMN = bcol;
+                BOMB_NUMBER = 10;
+                Log.d("LEVEL", level);
                 break;
             case "intermediate":
-                buildBoard(irow, icol);
+                ROW = irow;
+                COLUMN = icol;
+                BOMB_NUMBER = 40;
+                Log.d("LEVEL", level);
                 break;
             case "advanced":
-                buildBoard(arow, acol);
+                ROW = arow;
+                COLUMN = acol;
+                BOMB_NUMBER = 99;
+                Log.d("LEVEL", level);
                 break;
             default:
-                buildBoard(brow, bcol);
+                ROW = brow;
+                COLUMN = bcol;
+                BOMB_NUMBER = 10;
+                Log.d("LEVEL", "default");
                 break;
+        }
+        buildBoard();
+    }
+
+
+    public void buildBoard() {
+        holdBoard = findViewById(R.id.board_template);
+        holdBoard.removeAllViews(); // Clear previous views if any
+
+        // Create GridLayout instead of LinearLayout
+        GridLayout gridLayout = new GridLayout(this);
+        gridLayout.setColumnCount(COLUMN);
+        gridLayout.setRowCount(ROW);
+        gridLayout.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        ));
+        gridLayout.setBackgroundColor(Color.GRAY);
+
+        gameBoard = new Button[ROW][COLUMN];
+
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COLUMN; j++) {
+                Button button = new Button(this);
+                button.setLayoutParams(new ViewGroup.LayoutParams(120, 120));
+                button.setId(View.generateViewId());
+                button.setPadding(bsize, bsize, bsize, bsize);
+                button.setText("");
+                button.setOnClickListener(this::OnClickButton);
+                button.setTag(R.id.button_position_tag, new int[]{i, j});
+
+                gameBoard[i][j] = button;
+                gridLayout.addView(button);
+            }
+        }
+        holdBoard.addView(gridLayout);
+    }
+
+    // action after the button clicked
+    private void OnClickButton(View view) {
+        int[] pos = (int[]) view.getTag(R.id.button_position_tag);
+        int i = pos[0];
+        int j = pos[1];
+        if (!started) {
+            placeBomb(i, j);
+            started = true;
+            handler.post(runnable);
+        }
+        // if clicked cell was 0
+        if (board[i][j] == 0) {
+            setBoard(i, j, "update");
+        }
+        // if clicked cell was a bomb
+        else if (board[i][j] == -1) {
+            // stops the timer
+            handler.removeCallbacks(runnable);
+            // set bomb picture on the -1 cells
+            // disable buttons
+            for (int row = 0; row < ROW; row++) {
+                for (int col = 0; col < COLUMN; col++) {
+                    if (board[row][col] == -1) {
+                        gameBoard[row][col].setBackgroundResource(R.drawable.bomb);
+                    }
+                    gameBoard[row][col].setEnabled(false);
+                }
+            }
+            gameOver();
+        }
+        else {
+            gameBoard[i][j].setText(String.valueOf(board[i][j]));
+        }
+        checkWin();
+    }
+
+    // randomly place bombs
+    private void placeBomb(int row, int col) {
+        board = new int[ROW][COLUMN];
+        // store Array that stores all the positions of each bomb
+        ArrayList<ArrayList<Integer>> bombPosArray = new ArrayList<>();
+        Random rand = new Random();
+
+        // initialize board
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COLUMN; j++) {
+                board[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < BOMB_NUMBER; i++) {
+            int r = rand.nextInt(ROW);
+            int c = rand.nextInt(COLUMN);
+
+            // first clicked button can't be a bomb.
+            if (r != row && c != col) {
+                // set the bomb value as -1
+                board[r][c] = -1;
+                ArrayList<Integer> bombPos = new ArrayList<>();
+                bombPos.add(r);
+                bombPos.add(c);
+                // avoid same place
+                if (bombPosArray.contains(bombPos)) {
+                    i--;
+                }
+                else {
+                    bombPosArray.add(bombPos);
+                }
+            }
+            else {
+                // skipping this for loop by decrement i value
+                i--;
+            }
+        }
+        for (int k = 0; k < BOMB_NUMBER; k++) {
+            int r = bombPosArray.get(k).get(0);
+            int c = bombPosArray.get(k).get(1);
+            setBoard(r, c, "create");
         }
     }
 
-    public void buildBoard(int row, int col){
-        LinearLayout holdBoard = findViewById(R.id.board_template);
-        gameBoard = new Button[row][col];
+    // set the board
+    private void setBoard(int r, int c, String order) {
+        int istart = -1, iend = 1, jstart = -1, jend = 1;
 
-        for(int i = 0; i<row; i++){
-            LinearLayout gameRows = new LinearLayout(this);
-            gameRows.setOrientation(LinearLayout.HORIZONTAL);
-            for(int j = 0; j<col; j++){
-                Button button = new Button(this);
-                button.setLayoutParams(new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT
-                ));
+        // decide start point of loop and end point of loop so if the bomb
+        // is on the edge, it can ignore the elements that's out of the grid
+        if (r == 0) {
+             istart = 0;
+        }
+        if (r == ROW - 1) {
+            iend = 0;
+        }
+        if (c == 0) {
+            jstart = 0;
+        }
+        if (c == COLUMN - 1) {
+            jend = 0;
+        }
 
-                button.setPadding(bsize, bsize, bsize, bsize);
-                button.setText("");
-                button.setTag("button_"+i+"_"+j);
-
-                gameBoard[i][j] = button;
-                gameRows.addView(button);
+        if (order.equals("create")) {
+            // inclement the number of cells that's surrounding the bomb.
+            for (int i = istart; i <= iend; i++) {
+                for (int j = jstart; j <= jend; j++) {
+                    // make sure not to inclement bomb cell
+                    if (board[r + i][c + j] != -1) {
+                        board[r + i][c + j] += 1;
+                    }
+                }
             }
-            holdBoard.addView(gameRows);
+        }
+        else if (order.equals("update")) {
+            for (int i = istart; i <= iend; i++) {
+                for (int j = jstart; j <= jend; j++) {
+                    // make sure gameBoard is not updated yet
+                    if (gameBoard[r + i][c + j].getText() == "") {
+                        gameBoard[r + i][c + j].setText(String.valueOf(board[r + i][c + j]));
+                        if (board[r + i][c + j] == 0) {
+                            // use " " instead of "" so the text in a button can stay blank
+                            // but be able to classify if the button is revealed or not
+                            gameBoard[r + i][c + j].setText(" ");
+                            gameBoard[r + i][c + j].setBackgroundColor(Color.GRAY);
+                            setBoard(r + i, c + j, "update");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void gameOver() {
+        handler.removeCallbacks(runnable);
+        LinearLayout gameOverPopup = findViewById(R.id.GameOverPopup);
+        TextView gameOverMessage = findViewById(R.id.GameOverMessage);
+        gameOverMessage.setText("Game Over!");
+        gameOverPopup.setVisibility(View.VISIBLE);
+    }
+
+
+    private void OnClickAgain(View view) {
+        holdBoard.removeAllViews();
+        started = false;
+        seconds = new int[]{0};
+        timer.setText("");
+        buildBoard();
+        LinearLayout gameOverPopup = findViewById(R.id.GameOverPopup);
+        gameOverPopup.setVisibility(View.GONE);
+    }
+
+
+    private void OnClickBack(View view) {
+        Intent intermediateBoard = new Intent(this, PlaySoloActivity.class);
+        startActivity(intermediateBoard);
+//        setContentView(R.layout.play_solo_page);
+    }
+
+    private void checkWin() {
+        boolean won = true;
+
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COLUMN; j++) {
+                // If any non-bomb cell is still not revealed, player has not won yet
+                if (board[i][j] != -1 && gameBoard[i][j].getText().equals("")) {
+                    won = false;
+                    break;
+                }
+            }
+            if (!won) break;
+        }
+
+        if (won) {
+            // Stop the timer
+            handler.removeCallbacks(runnable);
+
+            // Show popup with "You Won!"
+            LinearLayout gameOverPopup = findViewById(R.id.GameOverPopup);
+            TextView gameOverMessage = findViewById(R.id.GameOverMessage);
+            String currentTime = timer.getText().toString();
+            gameOverMessage.setText("You Won! (" + currentTime + ")");
+            gameOverPopup.setVisibility(View.VISIBLE);
+
+            // Disable all buttons
+            for (int i = 0; i < ROW; i++) {
+                for (int j = 0; j < COLUMN; j++) {
+                    gameBoard[i][j].setEnabled(false);
+                }
+            }
         }
     }
 
